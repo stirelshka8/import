ef0bad71bac711a7613cd0fb7a13bcd8aaaa71c7
stirelshka8 <stirelshka8@gmail.com>
2022-10-29 15:13:47 +0300

    DZ end

---
+++ aplication/db/people.py
@@ -0,0 +1,2 @@
+def get_employees():
+    print("Вывод модуля get_employees")
---
+++ main.py
@@ -0,0 +1,6 @@
+from aplication.salary import calculate_salary
+from aplication.db.people import get_employees
+
+if __name__ == "__main__":
+    calculate_salary()
+    get_employees()
